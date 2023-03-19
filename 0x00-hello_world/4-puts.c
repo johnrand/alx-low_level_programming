cf641a5fcaf4f9d88f423376b0711b0c54d2edf5
johnrand <johnrandy0652@gmail.com>
2023-03-19 04:34:46 -0700

    Printing a text to output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * print to the output 
+ *
+ * return - zero
+ */
+
+int main(void)
+{
+	puts("Programming is like buillding a multilingual puzzle");
+
+	return (0);
+}
