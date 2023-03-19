ed55c0ee3b774d776e37b5d81888ee6e297f0833
johnrand <johnrandy0652@gmail.com>
2023-03-19 07:03:18 -0700

    Using printf to display text on output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: Using printf to display text
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+
+	return (0);
+}
