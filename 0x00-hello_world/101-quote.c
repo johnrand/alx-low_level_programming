9fd1d902f30f319cee8e282976f5056a4602d61f
johnrand <johnrandy0652@gmail.com>
2023-03-19 08:58:49 -0700

    Printing text on stanard error and return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: Prints to the standard error and returns zero.
+ *
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+	const char message[];
+
+	message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, message, sizeof(message) - 1);
+
+	return (1);
+}
