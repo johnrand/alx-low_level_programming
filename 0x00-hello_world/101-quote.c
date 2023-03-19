e94349e44f4423dc3b5da8c90faf6f05c442a4a8
johnrand <johnrandy0652@gmail.com>
2023-03-19 09:23:47 -0700

    Fix:adding a header file for write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,10 @@ int main(void)
 {
 	const char message[10];
 
-	message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[10];
+
+	message[10] = "and that piece of art is useful\" - Dora Korpar,
+	     2015-10-19\n";
 
 	write(STDERR_FILENO, message, sizeof(message) - 1);
 
