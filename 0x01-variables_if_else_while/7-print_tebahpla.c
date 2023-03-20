6370ce70bc10ee869697d47e29e4ac4d70defc39
johnrand <johnrandy0652@gmail.com>
2023-03-20 16:26:59 -0700

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; 1 >= 97; i--)
+	{
+		putchar('a' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
