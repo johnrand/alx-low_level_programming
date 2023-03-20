8b03a2e763259b95865ed9aea8657ebded225ad2
johnrand <johnrandy0652@gmail.com>
2023-03-20 15:56:30 -0700

    removing some letters from alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: Print if a number is positive or negative
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			/* exclude letters e and q */
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
