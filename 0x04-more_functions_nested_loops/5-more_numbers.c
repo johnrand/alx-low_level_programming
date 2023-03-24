c01893627cd140c20f9e65267a946a7ff57c0f55
johnrand <johnrandy0652@gmail.com>
2023-03-24 16:51:40 -0700

    More numbers: print a range of numbers 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 10 time the numbers from 0 to 14
+ * followed by a newline
+ * print most numbers
+ *
+ * Return: void
+ *
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
