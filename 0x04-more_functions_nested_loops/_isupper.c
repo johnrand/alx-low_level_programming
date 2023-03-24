9fa1e79cf6fa0e0a1e7d01e7e86bcd2bc5c9d8f1
johnrand <johnrandy0652@gmail.com>
2023-03-24 03:22:06 -0700

    First commit for more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/_isupper.c
@@ -0,0 +1,21 @@
+/**
+ * _isupper - checks for uppercase characters
+ *
+ * @c: character to check
+ * Function prototype for _isupper
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
