e9673bcf6231250c5bcae8b16a4476ad0972d828
johnrand <johnrandy0652@gmail.com>
2023-03-30 15:07:25 -0700

    fix:ensuring the correct variable is used all places

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,6 +4,7 @@
  * reverse_array - Reverses the content of an array of integers.
  * @a: The array of integers.
  * @n: The number of elements of the array.
+ * Return: void
  */
 void reverse_array(int *a, int n)
 {
@@ -14,6 +15,6 @@ void reverse_array(int *a, int n)
 	{
 		jnr = a[i];
 		a[i] = a[n - i - 1];
-		a[n - i - 1] = tmp;
+		a[n - i - 1] = jnr;
 	}
 }
