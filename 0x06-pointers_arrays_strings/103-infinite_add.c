1d123e5fd4978ab6fd0250f99cb22dfbcb914c80
johnrand <johnrandy0652@gmail.com>
2023-03-30 17:16:27 -0700

    fix the code to reduce lines

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
 
 /**
  * infinite_add - Adds two numbers
@@ -11,56 +13,52 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, len_res, carry, sum;
-	int i, j, k;
-
-	len1 = 0;
-	while (n1[len1] != '\0')
-		len1++;
-
-	len2 = 0;
-	while (n2[len2] != '\0')
-		len2++;
-
-	if (len1 >= size_r || len2 >= size_r)
+	int len1, len2, len_res, carry, sum, i, j;
+	
+	len1 = strlen(n1);
+	len2 = strlen(n2);
+	
+	if (len1 > size_r - 1 || len2 > size_r - 1)
+	{
 		return (0);
-
+	}
+	
 	carry = 0;
 	len_res = 0;
 	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
 	{
 		sum = carry;
 		if (i >= 0)
+		{
 			sum += n1[i] - '0';
+		}
 		if (j >= 0)
+		{
 			sum += n2[j] - '0';
-
-		if (len_res >= size_r - 1)
-			return (0);
-
-		r[len_res] = sum % 10 + '0';
-		len_res++;
+		}
+		
+		r[len_res++] = (sum % 10) + '0';
 		carry = sum / 10;
 	}
-
-	if (carry != 0)
+	
+	if (carry && len_res < size_r - 1)
 	{
-		if (len_res >= size_r - 1)
-			return (0);
-
-		r[len_res] = carry + '0';
-		len_res++;
+		r[len_res++] = carry + '0';
 	}
-
+	
+	if (len_res >= size_r)
+	{
+		return (0);
+	}
+	
 	r[len_res] = '\0';
-
-	for (i = 0, k = len_res - 1; i < k; i++, k--)
+	
+	for (i = 0, j = len_res - 1; i < j; i++, j--)
 	{
 		char tmp = r[i];
-		r[i] = r[k];
-		r[k] = tmp;
+		r[i] = r[j];
+		r[j] = tmp;
 	}
-
+	
 	return (r);
 }
-
