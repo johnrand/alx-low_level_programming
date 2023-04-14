2cfa31bacebfa5cce5e4a1bd4420ecd499825872
johnrand <johnrandy0652@gmail.com>
2023-04-14 16:05:44 -0700

    Fix: compile error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,54 +1,53 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _realloc - Reallocates a memory block using malloc and free.
- * @ptr: A pointer to the memory previously allocated with a call to malloc.
- * @old_size: The size, in bytes, of the allocated space for ptr.
- * @new_size: The new size, in bytes, of the new memory block.
+ * _realloc - reallocates a memory block using malloc and free.
  *
- * Return: A pointer to the reallocated memory block.
+ * @ptr: pointer to the memory previously allocated with malloc(old_size)
+ * @old_size: size in bytes of the allocated space for ptr
+ * @new_size: new size in bytes of the new memory block
+ *
+ * Return: Pointer to the new memory block
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *new_ptr, *prev_ptr;
+	char *new_ptr;
 	unsigned int i;
 
 	if (new_size == old_size)
 		return (ptr);
 
-	if (!ptr)
+	if (ptr == NULL)
 	{
 		new_ptr = malloc(new_size);
-		if (!new_ptr)
+		if (new_ptr == NULL)
 			return (NULL);
 		return (new_ptr);
 	}
 
-	if (!new_size)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
 	new_ptr = malloc(new_size);
-	if (!new_ptr)
+	if (new_ptr == NULL)
 		return (NULL);
 
-	prev_ptr = ptr;
-	if (new_size < old_size)
+	if (new_size > old_size)
 	{
-		for (i = 0; i < new_size; i++)
-			new_ptr[i] = prev_ptr[i];
+		for (i = 0; i < old_size; i++)
+			new_ptr[i] = ((char *) ptr)[i];
 	}
 	else
 	{
-		for (i = 0; i < old_size; i++)
-			new_ptr[i] = prev_ptr[i];
+		for (i = 0; i < new_size; i++)
+			new_ptr[i] = ((char *) ptr)[i];
 	}
 
 	free(ptr);
-
 	return (new_ptr);
 }
