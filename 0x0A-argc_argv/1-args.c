11f3566f305d74c993f800d24926950fc39d0e80
johnrand <johnrandy0652@gmail.com>
2023-04-15 20:03:44 -0700

    corecting main header

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,8 @@
  * Return: always(0)
  */
 
-int main(int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
+int main(int argc __attribute__((unused)),
+		char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
