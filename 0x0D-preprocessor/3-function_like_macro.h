7995481e6e597306e9029f14e3ba4b4c794a47ea
johnrand <johnrandy0652@gmail.com>
2023-04-17 14:58:03 -0700

    macro that computs absolute value of a number

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,18 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+/**
+ * ABS - Macro that computes the absolute value of a number x
+ * @x: The input number
+ *
+ * Description: This macro takes a number x and computes its absolute value.
+ *              It uses a ternary operator to determine if x is negative,
+ *              in which case it returns the negation of x, otherwise it
+ *              returns x itself.
+ *
+ * Return: The absolute value of x
+ */
+
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
