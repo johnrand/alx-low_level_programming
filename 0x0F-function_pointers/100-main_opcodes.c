7c403df39406d6a21b9aac5a5cc96039ae6fed6d
johnrand <johnrandy0652@gmail.com>
2023-04-21 13:06:31 -0700

    prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of arguments
+ * @argv: Array of argument strings
+ *
+ * Return: 0 on success, 1 if incorrect number of arguments
+ * 2 if negative bytes
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int bytes;
+	unsigned char *opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	*opcode = (unsigned char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x ", opcode[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
