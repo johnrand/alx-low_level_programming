f889f1e9c5fc4a4b81d995a783be84175d391b5b
johnrand <johnrandy0652@gmail.com>
2023-04-21 12:27:47 -0700

    four file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char *operator;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(operator);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,77 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	if (b == 0)
+	{
+		_putchar('Error\n');
+		exit(100);
+	}
+
+	return (a * b);
+}
+
+/**
+ * op_div - Divides two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of dividing a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Computes the remainder of division of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Remainder of division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		_putchar('Error\n');
+		exit(100);
+	}
+	return (a % b);
+}
