3b804b2bf66f42844c75942331994c31816c914f
johnrand <johnrandy0652@gmail.com>
2023-05-02 06:22:09 -0700

    fix

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,14 +10,15 @@
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
+	listint_t *current = *head;
 	listint_t *next = NULL;
 
-	while (*head != NULL)
+	while (*current != NULL)
 	{
-		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = next;
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
 
 	*head = prev;
