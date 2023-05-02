f677d39aecd86c8d3946b6f3b52a578f9b6e2396
johnrand <johnrandy0652@gmail.com>
2023-05-02 07:18:08 -0700

    update dog header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,8 +18,15 @@ struct dog
 	char *owner;
 };
 
+/* function prototypes */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
 
 #endif /* DOG_H */
