bf8a8e94656252518baf187c03b79c1cb286dfbd
johnrand <johnrandy0652@gmail.com>
2023-05-08 16:14:00 -0700

    copies content of a file to another

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,6 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+int main(int argc, char **argv);
+
 #endif /* MAIN_H */
