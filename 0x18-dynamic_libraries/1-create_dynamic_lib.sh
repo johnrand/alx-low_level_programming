63f6fc56c1b8cf414c6ba30a7d34ef22ff937788
johnrand <johnrandy0652@gmail.com>
2024-01-16 04:16:06 +0300

    Initial Commit

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -4,7 +4,7 @@
 gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c
 
 # Create a dynamic library from all .o files
-ar -shared -o liball.so *.o
+gcc -shared -o liball.so *.o
 
 # remove the object files creted
 rm *.o
