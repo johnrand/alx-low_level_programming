544d73dbd26684f6f4a0c24de049c7d5effe1084
johnrand <johnrandy0652@gmail.com>
2024-02-07 19:47:30 +0300

    Fixing some errors

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,11 +20,15 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
+			if (j != 0)
+			{
+				printf(" ");
+			}
 			printf("%3d", i * j);
 
 			if (j < n)
 			{
-				printf(",");
+				printf(", ");
 			}
 		}
 		printf("\n");
